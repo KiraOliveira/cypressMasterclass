77b59075211471d63f557399265583249afdf17e
Kira Oliveira <kira.oliveira.web@gmail.com>
2025-08-08 04:23:18 -0300

    Criando README.md

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# cypressMasterclass
-Masterclass realizada no dia 07/08 - Framework usado Cypress
+## Masterclass com o Framework Cypress - 07/08 - Fernado Papito
+
+Repositório com o que aprendi na Masterclass com o Fernando Papito
+
+* Utilizando o framewrok [Cypress](https://www.cypress.io/).
+
+---
+
+## Sobre o repositório
+
+O site [Walkdog](https://walkdog.vercel.app/) foi utilizado para realizar a automação da interface web.
+
+---
+
+## Tecnologias Utilizadas
+
+- Cypress 14.x
+- Cypress-Cucumber-Preprocessor
+- Cypress-XPath
+- JavaScript
+- Gherkin
+- VS Code
+
+---
+
+## Estrutura do Projeto
+
+```
+CYPRESSMASTERCLASS/
+├── cypress/
+│   ├── e2e/
+│   │   └── ├── cadastDescribe.cy.js
+|   |       ├── cadastro.cy.js
+|   |       └── cadastroComando.cy.js
+|   ├── fixtures/
+│   │       ├── rg.png
+├── package.json
+└── README.md
+```
+
+---
+
+## Como Realizar o clone do Repositório
+
+```bash
+git clone git@github.com:KiraOliveira/cypressMasterclass.git
+cd cypressMasterclass
+```
+
+---
+
+## Como Executar os Testes
+
+```bash
+npm install cypress
+npx cypress open ou npx cypress run(heandless)
+```
+
+---
+
+## Execução dos testes
+
+[![Execução dos Testes]](https://github.com/user-attachments/assets/96366a67-9a76-4c53-afe6-c885ee1df527)
+
+
---
+++ cypress/e2e/cadastDescribe.cy.js
@@ -0,0 +1,50 @@
+describe("Cadastro", () => {
+
+      // Massa a ser utilizada  
+      const walker = {
+        name: 'Kira Oliveira',
+        email: 'kira@walkdog.com',
+        cpf: '79788201067',
+        cep: '87210296',
+        number: '2',
+        info: 'Apartamento 3',
+        document: 'rg.png'
+    }
+  
+  // Cadastro realizado com sucesso  
+  it("Deve poder cadastrar um novo walker com sucesso", () => {
+   
+    cy.submitSignupForm(walker)
+
+    // Validando que o cadastro foi realizado com sucesso
+    cy.get('#swal2-html-container')
+      .should('be.visible')
+      .and('have.text', 'Recebemos o seu cadastro e em breve retornaremos o contato.')
+     
+    cy.contains('button', 'Voltar')
+      .click()  
+        
+  });
+
+  // Erro ao inserir um e-mail incorreto
+  it("Não deve cadastrar com e-mail incorreto", () => {
+
+    cy.submitSignupForm({...walker, email:'www.teste.com.br'}) // Usando os '...' estamos espalhamento de objetos dentro das {}
+
+    // Validando que exibiu a mensagem de erro abaixo do campo e-mail
+    cy.get('.alert-error')
+      .should('be.visible')
+      .and('have.text', 'Informe um email válido')
+  });
+
+  it("Não deve cadastrar quando o número for menor que '0'", () => {
+
+    cy.submitSignupForm({...walker, number:'0'}) // Usando os '...' estamos espalhamento de objetos dentro das {}
+
+    // Validando que exibiu a mensagem de erro abaixo do campo número
+    cy.get('.alert-error')
+      .should('be.visible')
+      .and('have.text', 'Informe um número maior que zero')
+  });
+});
+
